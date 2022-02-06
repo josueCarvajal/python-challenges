6c4af1b32222400df0c43ea009258580cfc0ee34
Josue Carvajal <josue.crs97@gmail.com>
2022-02-05 19:22:47 -0600

    fixed a bug in the jumpingOnTheClouds challenge

--- JumpingOnTheClouds/Jumping.py
+++ JumpingOnTheClouds/Jumping.py
@@ -9,9 +9,14 @@ def calculate_jumps(array):
 
    while step_index <= len(array):
        step_index = index + 2
+       array_lenght = len(array)-1
        
-       if step_index >= len(array):
-           break
+       if step_index > array_lenght:
+            # there is one position left in the array
+            if (step_index - array_lenght) == 1:
+                if array[array_lenght] == 0:
+                   jumps = jumps + 1
+            break
 
        if array[step_index] == 1:
            step_index = step_index - 1;
@@ -26,5 +31,4 @@ def calculate_jumps(array):
 
 if __name__ == '__main__':
     array = [0,1,0,0,0,1,0]
-    #array = [0,0,0,0,1,0]
     print(calculate_jumps(array))
--- JumpingOnTheClouds/UnitTest.py
+++ JumpingOnTheClouds/UnitTest.py
@@ -15,7 +15,14 @@ class testJumpClouds(unittest.TestCase):
     def test_third_jump(self):
         array = [0,0,1,0,0,1,0]
         self.assertEqual(calculate_jumps(array), 4, 'Expected output: 4')
+    
+    def test_fourth_jump(self):
+        array = [0,0,0,1,0,0]
+        self.assertEqual(calculate_jumps(array), 3, 'Expected output: 3')
 
+    def test_fifth_jump(self):
+        array = [0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,1,0,1,0,0,0,1,0,0,1,0,0,0,1,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,1,0,0]
+        self.assertEqual(calculate_jumps(array), 28, 'Expected output: 28')
 
 
 if __name__ == '__main__':
